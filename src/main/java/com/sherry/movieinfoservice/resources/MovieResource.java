07c07f12cb8e7b6c23b510477c47ae1bb01f2e44
Shehroaz <muhammadshehroaz@gmail.com>
2021-02-10 13:32:39 +0500

    configuring spring cloud and added client to eureka server.

--- src/main/java/com/sherry/movieinfoservice/resources/MovieResource.java
+++ src/main/java/com/sherry/movieinfoservice/resources/MovieResource.java
@@ -11,6 +11,6 @@ public class MovieResource {
 
     @RequestMapping("/{movieId}")
     public Movie getMoviesInfo (@PathVariable("movieId") String movieId){
-        return new Movie(movieId , "test name");
+        return new Movie(movieId , "Movie name ");
     }
 }
